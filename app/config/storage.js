504481072d6fdc802335cf91d5f9bd2d774315d7
Chris Cole <whitewaterdesign>
2025-01-17 17:12:16 +0000

    build: 488807 - Migrate to managed identity

--- app/config/storage.js
+++ app/config/storage.js
@@ -6,7 +6,8 @@ const schema = Joi.object({
   connectionString: Joi.string().when('useConnectionStr', { is: true, then: Joi.required(), otherwise: Joi.allow('').optional() }),
   storageAccount: Joi.string().required(),
   attachmentsContainer: Joi.string().default('attachments'),
-  useConnectionString: Joi.boolean().default(false)
+  useConnectionString: Joi.boolean().default(false),
+  managedIdentityClientId: Joi.string().optional()
 })
 
 // Build config
@@ -14,7 +15,8 @@ const config = {
   connectionString: getEnvironmentVariable('AZURE_STORAGE_CONNECTION_STRING'),
   storageAccount: getEnvironmentVariable('AZURE_STORAGE_ACCOUNT_NAME'),
   attachmentsContainer: getEnvironmentVariable('AZURE_STORAGE_ATTACHMENTS_CONTAINER'),
-  useConnectionString: getEnvironmentVariable('AZURE_STORAGE_USE_CONNECTION_STRING')
+  useConnectionString: getEnvironmentVariable('AZURE_STORAGE_USE_CONNECTION_STRING'),
+  managedIdentityClientId: getEnvironmentVariable('AZURE_CLIENT_ID')
 }
 
 // Validate config
