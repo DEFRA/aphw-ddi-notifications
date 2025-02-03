7cf3911dc7bf86460200d326cd025b8c0ef14937
work <jeremy.barnsley@esynergy.co.uk>
2025-02-03 15:54:58 +0000

    Full coverage

--- app/index.js
+++ app/index.js
@@ -1,9 +1,8 @@
-/** instanbul ignore **/
-
 require('./insights').setup()
 require('log-timestamp')
 const { start, stop } = require('./messaging/inbound/notification')
 
+/* istanbul ignore next */
 process.on(['SIGTERM', 'SIGINT'], async () => {
   await stop()
   process.exit(0)
